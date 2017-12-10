e11a527392fb4ff85a70635d2f31d8388ac9db47
A Bravo Dev <abravodev@users.noreply.github.com>
2017-12-10 17:05:29 +0100

    Add possiblity of alias to select query

--- SqlQueryBuilder/src/SelectQueryElements/SelectQuery.java
+++ SqlQueryBuilder/src/SelectQueryElements/SelectQuery.java
@@ -2,20 +2,26 @@ package SelectQueryElements;
 
 public class SelectQuery implements ISelectQuery {
 
-    private final String previousQuery;
-    private String selectClause;
+	private final String previousQuery;
+	private String selectClause;
 
-    public SelectQuery(String selectQuery) {
-        this.previousQuery = selectQuery;
-    }
+	public SelectQuery(String selectQuery) {
+		this.previousQuery = selectQuery;
+	}
 
-    public JoinableQuery from(String table){
-        selectClause = String.format("FROM %s", table);
-        return fromQuery();
-    }
+	public JoinableQuery from(String table){
+		selectClause = String.format("FROM %s", table);
+		return fromQuery();
+	}
 
-    private JoinableQuery fromQuery(){
-        String formattedQuery = String.format("%s %s", previousQuery, selectClause);
-        return new JoinableQuery(formattedQuery);
-    }
+	public JoinableQuery from(String table, String alias){
+		selectClause = String.format("FROM %s %s", table, alias);
+		return fromQuery();
+	}
+
+	private JoinableQuery fromQuery(){
+		String formattedQuery = String.format("%s %s", previousQuery, selectClause);
+		return new JoinableQuery(formattedQuery);
+	}
+
 }
--- SqlQueryBuilderTest/src/SqlSelectQueryBuilderTest.java
+++ SqlQueryBuilderTest/src/SqlSelectQueryBuilderTest.java
@@ -5,126 +5,136 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SqlSelectQueryBuilderTest {
 
-    private SqlQueryBuilder builder;
+	private SqlQueryBuilder builder;
 
-    @BeforeEach
-    void setUp() {
-        builder = new SqlQueryBuilder();
-    }
+	@BeforeEach
+	void setUp() {
+		builder = new SqlQueryBuilder();
+	}
 
-    @Test
-    void simpleSelect(){
-        String query = builder
-                .select("FirstName", "LastName")
-                .from("Users")
-                .query();
-        String writtenQuery = "SELECT FirstName, LastName FROM Users";
-        assertEquals(writtenQuery, query);
-    }
+	@Test
+	void simpleSelect(){
+		String query = builder
+			.select("FirstName", "LastName")
+			.from("Users")
+			.query();
+		String writtenQuery = "SELECT FirstName, LastName FROM Users";
+		assertEquals(writtenQuery, query);
+	}
 
-    @Test
-    void emptySelect(){
-        String query = builder
-                .select()
-                .from("Users")
-                .query();
-        String writtenQuery = "SELECT * FROM Users";
-        assertEquals(writtenQuery, query);
-    }
+	@Test
+	void emptySelect(){
+		String query = builder
+			.select()
+			.from("Users")
+			.query();
+		String writtenQuery = "SELECT * FROM Users";
+		assertEquals(writtenQuery, query);
+	}
 
-    @Test
-    void selectWithWhereClause(){
-        String query = builder
-                .select()
-                .from("Users")
-                .where("LastName = 'Doe'")
-                .query();
-        String writtenQuery = "SELECT * FROM Users WHERE LastName = 'Doe'";
-        assertEquals(writtenQuery, query);
-    }
+	@Test
+	void selectWithWhereClause(){
+		String query = builder
+			.select()
+			.from("Users")
+			.where("LastName = 'Doe'")
+			.query();
+		String writtenQuery = "SELECT * FROM Users WHERE LastName = 'Doe'";
+		assertEquals(writtenQuery, query);
+	}
 
-    @Test
-    void selectWithOrderByClause(){
-        String query = builder
-                .select()
-                .from("Users")
-                .orderBy("LastName")
-                .query();
-        String writtenQuery = "SELECT * FROM Users ORDER BY LastName";
-        assertEquals(writtenQuery, query);
-    }
+	@Test
+	void selectWithAlias(){
+		String query = builder
+			.select("u.FirstName")
+			.from("Users", "u")
+			.query();
+		String writtenQuery = "SELECT u.FirstName FROM Users u";
+		assertEquals(writtenQuery, query);
+	}
 
-    @Test
-    void selectWithWhereInnerJoinAndOrderBy(){
-        String query = builder
-                .select()
-                .from("Users")
-                .innerJoin("Clients", "ClientID", "ID")
-                .whereEqual("LastName", "Doe")
-                .orderBy("FirstName")
-                .query();
-        String writtenQuery = "SELECT * FROM Users INNER JOIN Clients ON ClientID=ID WHERE LastName='Doe' ORDER BY FirstName";
-        assertEquals(writtenQuery, query);
-    }
+	@Test
+	void selectWithOrderByClause(){
+		String query = builder
+			.select()
+			.from("Users")
+			.orderBy("LastName")
+			.query();
+		String writtenQuery = "SELECT * FROM Users ORDER BY LastName";
+		assertEquals(writtenQuery, query);
+	}
 
-    @Test
-    void selectWithWhereInnerJoinAndGroupBy(){
-        String query = builder
-                .select()
-                .from("Users")
-                .innerJoin("Clients", "ClientID", "ID")
-                .whereEqual("LastName", "Doe")
-                .groupBy("FirstName")
-                .query();
-        String writtenQuery = "SELECT * FROM Users INNER JOIN Clients ON ClientID=ID WHERE LastName='Doe' GROUP BY FirstName";
-        assertEquals(writtenQuery, query);
-    }
+	@Test
+	void selectWithWhereInnerJoinAndOrderBy(){
+		String query = builder
+			.select()
+			.from("Users")
+			.innerJoin("Clients", "ClientID", "ID")
+			.whereEqual("LastName", "Doe")
+			.orderBy("FirstName")
+			.query();
+		String writtenQuery = "SELECT * FROM Users INNER JOIN Clients ON ClientID=ID WHERE LastName='Doe' ORDER BY FirstName";
+		assertEquals(writtenQuery, query);
+	}
 
-    @Test
-    void selectWithInnerJoinAndGroupBy(){
-        String query = builder
-                .select()
-                .from("Users")
-                .innerJoin("Clients", "ClientID", "ID")
-                .groupBy("FirstName")
-                .query();
-        String writtenQuery = "SELECT * FROM Users INNER JOIN Clients ON ClientID=ID GROUP BY FirstName";
-        assertEquals(writtenQuery, query);
-    }
+	@Test
+	void selectWithWhereInnerJoinAndGroupBy(){
+		String query = builder
+			.select()
+			.from("Users")
+			.innerJoin("Clients", "ClientID", "ID")
+			.whereEqual("LastName", "Doe")
+			.groupBy("FirstName")
+			.query();
+		String writtenQuery = "SELECT * FROM Users INNER JOIN Clients ON ClientID=ID WHERE LastName='Doe' GROUP BY FirstName";
+		assertEquals(writtenQuery, query);
+	}
 
-    @Test
-    void selectWithInnerJoinGroupByAndOrderBy(){
-        String query = builder
-                .select()
-                .from("Users")
-                .innerJoin("Clients", "ClientID", "ID")
-                .groupBy("FirstName")
-                .orderBy("LastName")
-                .query();
-        String writtenQuery = "SELECT * FROM Users INNER JOIN Clients ON ClientID=ID GROUP BY FirstName ORDER BY LastName";
-        assertEquals(writtenQuery, query);
-    }
+	@Test
+	void selectWithInnerJoinAndGroupBy(){
+		String query = builder
+			.select()
+			.from("Users")
+			.innerJoin("Clients", "ClientID", "ID")
+			.groupBy("FirstName")
+			.query();
+		String writtenQuery = "SELECT * FROM Users INNER JOIN Clients ON ClientID=ID GROUP BY FirstName";
+		assertEquals(writtenQuery, query);
+	}
 
-    @Test
-    void innerJoinAndInnerJoin(){
-        String query = builder
-                .select()
-                .from("Users")
-                .innerJoin("Clients", "ClientID", "ID")
-                .innerJoin("Tickets", "UserID", "ID")
-                .query();
-        String writtenQuery = "SELECT * FROM Users INNER JOIN Clients ON ClientID=ID INNER JOIN Tickets ON UserID=ID";
-        assertEquals(writtenQuery, query);
-    }
+	@Test
+	void selectWithInnerJoinGroupByAndOrderBy(){
+		String query = builder
+			.select()
+			.from("Users")
+			.innerJoin("Clients", "ClientID", "ID")
+			.groupBy("FirstName")
+			.orderBy("LastName")
+			.query();
+		String writtenQuery = "SELECT * FROM Users INNER JOIN Clients ON ClientID=ID GROUP BY FirstName ORDER BY LastName";
+		assertEquals(writtenQuery, query);
+	}
 
-    @Test
-    void selectFromAndTwoOrders(){
-        String query = builder
-                .select()
-                .from("Users")
-                .orderBy("LastName", "FirstName")
-                .query();
-        String writtenQuery = "SELECT * FROM Users ORDER BY LastName, FirstName";
-        assertEquals(writtenQuery, query);
-    }
+	@Test
+	void innerJoinAndInnerJoin(){
+		String query = builder
+			.select()
+			.from("Users")
+			.innerJoin("Clients", "ClientID", "ID")
+			.innerJoin("Tickets", "UserID", "ID")
+			.query();
+		String writtenQuery = "SELECT * FROM Users INNER JOIN Clients ON ClientID=ID INNER JOIN Tickets ON UserID=ID";
+		assertEquals(writtenQuery, query);
+	}
+
+	@Test
+	void selectFromAndTwoOrders(){
+		String query = builder
+			.select()
+			.from("Users")
+			.orderBy("LastName", "FirstName")
+			.query();
+		String writtenQuery = "SELECT * FROM Users ORDER BY LastName, FirstName";
+		assertEquals(writtenQuery, query);
+	}
 }
